f636e2cc7ff2a82a3e2a788bfd2c8231b1a0f563
Malaya Manjula <mlover24@gmail.com>
2020-10-31 23:21:53 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -10,7 +10,7 @@ app.use('/.netlify/functions/server', router);
 app.use(express.static(path.join(__dirname,'../public')));
 
 
-app.get('*', function (req, res) {    
+app.get('/', function (req, res) {    
     res.sendFile(path.join(__dirname,'../public/index.html'));
 });
 module.exports = app;
