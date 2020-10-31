20cb577c3edb094c436213a44519a835b1b22299
Malaya Manjula <mlover24@gmail.com>
2020-10-31 22:59:18 +0530

    updated paths

--- express/server.js
+++ express/server.js
@@ -1,15 +1,16 @@
 'use strict';
-var express = require('express');
+const express = require('express');
 const serverless = require('serverless-http');
+const path = require('path');
 
 const router = express.Router();
 
 var app = express();
-app.use(express.static(__dirname+'../public'));
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.use('/.netlify/functions/server', router)
 app.get('*', function (req, res) {    
-    res.sendFile(__dirname+'../public/index.html')
+    res.sendFile(path.join(__dirname,'../public/index.html'));
 });
 module.exports = app;
 module.exports.handler = serverless(app);
