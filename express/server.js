2eecdb831cad5145ab4c25a71a35bb1888a02ac9
Malaya Manjula <mlover24@gmail.com>
2020-11-01 18:41:52 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'../public')));
 app.use('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
-app.use('/about', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.use('/*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 module.exports = app;
 
