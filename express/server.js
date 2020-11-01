597ac2181c281abbbc9c0a13f1a893a17ee68777
Malaya Manjula <mlover24@gmail.com>
2020-11-01 07:41:37 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const app = express();
 
 
-app.use('/.netlify/functions/server', router);
+
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 module.exports = app;
 module.exports.handler = serverless(app);
