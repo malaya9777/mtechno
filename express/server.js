f6d2a614b319ddd3c2b0693339fa29c4c6519bfc
Malaya Manjula <mlover24@gmail.com>
2020-11-01 18:40:12 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,12 +1,12 @@
 'use strict';
 const express = require('express');
 const path = require('path');
-const serverless = require('serverless-http');
 const app = express();
-const router = express.Router();
+
 
 app.use(express.static(path.join(__dirname,'../public')));
 app.use('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.use('/about', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 module.exports = app;
-module.exports.handler = serverless(app);
+
