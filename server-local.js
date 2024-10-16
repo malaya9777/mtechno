50528f18ad0f721e192c258ae739ebc55395f8b6
Malaya Manjula <mlover24@gmail.com>
2024-10-17 01:08:03 +0530

    modified for render

--- server-local.js
+++ server-local.js
@@ -3,7 +3,7 @@ const ejs = require('ejs')
 const expressLayouts = require('express-ejs-layouts')
 const app = express();
 const path = require('path')
-
+const port = process.env.PORT || 4000;
 //set view
 app.use(expressLayouts);
 app.engine('html',ejs.renderFile);
@@ -17,4 +17,4 @@ app.use('',d_router);
 
 
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(port, () => console.log(`Local app listening on port ${port}!`));
